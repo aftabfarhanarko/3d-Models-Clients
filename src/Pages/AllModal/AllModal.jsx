ebb9f57c17ab4cd08e2bbfb855798ec4865afaba
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-08 20:07:08 +0600

    addes folders

--- src/Pages/AllModal/AllModal.jsx
+++ src/Pages/AllModal/AllModal.jsx
@@ -11,23 +11,74 @@ const AllModal = () => {
 
   useEffect(() => {
     setLoder(true);
-
     axio.get("/model").then((data) => {
       console.log(data.data);
       setCards(data.data);
-    setLoder(false);
-
+      setLoder(false);
     });
   }, [axio]);
- 
-    if(loder){
-    return <Loder></Loder>
+
+  // search
+  const handelSearch = (e) => {
+    e.preventDefault();
+    const searchText = e.target.text.value;
+    console.log('Search clicked now',{searchText});
+    axio.get("/search")
+    .then(result => {
+      console.log(result.data);
+      
+    })
+  }
+
+
+
+  if (loder) {
+    return <Loder></Loder>;
   }
   return (
     <div className="mt-25">
-      <h1 className="text-2xl text-center my-10 font-semibold">
-        All 3D Models{" "}
-      </h1>
+      <div className=" flex flex-col md:flex-row justify-between items-center mb-10 md:mb-4">
+        <h1 className="text-2xl text-center my-10 font-semibold">
+          All 3D Models{" "}
+        </h1>
+        <form onSubmit={handelSearch} className="relative -mt-5 md:mt-0 max-w-sm">
+          <input
+          name="text"
+            type="text"
+            placeholder="Search..."
+            className="w-full py-3 pl-12 pr-16 rounded-xl border border-gray-300 
+               focus:border-blue-500 focus:ring-2 focus:ring-blue-300 
+               transition-all duration-200 outline-none text-sm"
+          />
+
+          {/* Search Icon */}
+          <svg
+            className="w-5 h-5 absolute top-1/2 left-4 -translate-y-1/2 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+
+          {/* Right Button */}
+          <button type="submit"
+            className="absolute py-3 top-1/2 right-0 -translate-y-1/2 px-4  
+               bg-blue-600 text-white rounded-lg text-sm 
+               hover:bg-blue-700 active:scale-95 transition"
+          >
+            Search
+          </button>
+        </form>
+      </div>
       <div className="grid grid-cols-2  md:grid-cols-4 gap-7 px-2">
         <Suspense fallback={<FadeLoader color="#13f759" />}>
           {cards.map((model) => (
--- src/Pages/Letas-Model/LetesModel.jsx
+++ src/Pages/Letas-Model/LetesModel.jsx
@@ -8,24 +8,24 @@ const LetesModel = () => {
   const [modelsa, setModel] = useState([]);
   const [loder, setLoder] = useState(false);
 
-
   useEffect(() => {
-     setLoder(true);
+    setLoder(true);
     axio.get("/limet").then((data) => {
       console.log("This is Data", data.data);
       setModel(data.data);
-       setLoder(false);
+      setLoder(false);
     });
   }, [axio]);
   console.log(modelsa);
 
-
-    if(loder){
-    return <Loder></Loder>
+  if (loder) {
+    return <Loder></Loder>;
   }
   return (
     <div>
-        <h1 className="text-2xl text-center my-10 font-semibold">Featured 3D Models  6 Latest Uploaded</h1>
+      <h1 className="text-2xl text-center my-10 font-semibold">
+        Featured 3D Models 6 Latest Uploaded
+      </h1>
       <div className="grid grid-cols-2  md:grid-cols-4 gap-7 px-2">
         <Suspense fallback={<h1>Loding.......</h1>}>
           {modelsa.map((model) => (
